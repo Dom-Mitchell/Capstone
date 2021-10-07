0a0c69ed333ee1142a5eb4ce30829dd4b401e8b3
Domenick Mitchell <domenickmitchell00@gmail.com>
2021-10-07 09:55:29 -0400

    Removed Commented Code

--- ClientApp/src/components/form.tsx
+++ ClientApp/src/components/form.tsx
@@ -1,39 +1,39 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-export const FormContext = React.createContext({
-  form: {}
-});
+// export const FormContext = React.createContext({
+//   form: {}
+// });
 
-export function Form(props: { children: any; submit?: (() => void) | undefined; initialValues: any; }) {
-  const { children, submit = () => {}, initialValues } = props;
+// export function Form(props: { children: any; submit?: (() => void) | undefined; initialValues: any; }) {
+//   const { children, submit = () => {}, initialValues } = props;
 
-  const [form, setForm] = useState(initialValues);
+//   const [form, setForm] = useState(initialValues);
 
-  const handleFormChange = (event: { target: { name: any; value: any; }; }) => {
-    // Get the name of the field that caused this change event
-    // Get the new value of this field
-    const { name, value } = event.target;
+//   const handleFormChange = (event: { target: { name: any; value: any; }; }) => {
+//     // Get the name of the field that caused this change event
+//     // Get the new value of this field
+//     const { name, value } = event.target;
 
-    // Update state
-    // Assign new value to the appropriate form field
-    setForm({
-      ...form,
-      [name]: value
-    });
-  };
+//     // Update state
+//     // Assign new value to the appropriate form field
+//     setForm({
+//       ...form,
+//       [name]: value
+//     });
+//   };
 
-  return (
-    <form className="Form">
-      <FormContext.Provider value={{
-        form,
-        handleFormChange
-      }}>
-        {children}
-      </FormContext.Provider>
+//   return (
+//     <form className="Form">
+//       <FormContext.Provider value={{
+//         form,
+//         handleFormChange
+//       }}>
+//         {children}
+//       </FormContext.Provider>
 
-      <button type="button" onClick={() => submit(form)}>
-        Submit
-      </button>
-    </form>
-  );
-}
+//       <button type="button" onClick={() => submit(form)}>
+//         Submit
+//       </button>
+//     </form>
+//   );
+// }
--- ClientApp/src/components/formInput.tsx
+++ ClientApp/src/components/formInput.tsx
@@ -1,25 +1,25 @@
-import React, { useContext } from 'react'
-import { FormContext } from './form'
+// import React, { useContext } from 'react'
+// import { FormContext } from './form'
 
-export function FormInput(props: {
-  label: any
-  type?: 'text' | undefined
-  name: any
-}) {
-  const { label, type = 'text', name } = props
+// export function FormInput(props: {
+//   label: any
+//   type?: 'text' | undefined
+//   name: any
+// }) {
+//   const { label, type = 'text', name } = props
 
-  const formContext = useContext(FormContext)
-  const { form, handleFormChange } = formContext
+//   const formContext = useContext(FormContext)
+//   const { form, handleFormChange } = formContext
 
-  return (
-    <div className="FormInput">
-      <label>{label}</label>
-      <input
-        type={type}
-        name={name}
-        value={form[name]}
-        onChange={handleFormChange}
-      />
-    </div>
-  )
-}
+//   return (
+//     <div className="FormInput">
+//       <label>{label}</label>
+//       <input
+//         type={type}
+//         name={name}
+//         value={form[name]}
+//         onChange={handleFormChange}
+//       />
+//     </div>
+//   )
+// }
